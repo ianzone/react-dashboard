8cc1d7159e38345f57247334b3cf0483ff2e7c0a
ian <yihengz@outlook.com>
2024-07-22 15:06:57 +0800

    refactor: 💡 spinner

--- src/contexts/UIProvider.tsx
+++ src/contexts/UIProvider.tsx
@@ -1,83 +1,83 @@
 import { StyleProvider } from '@ant-design/cssinjs';
 import { App, ConfigProvider, theme } from 'antd';
 import {
-  type Dispatch,
-  type ReactNode,
-  type SetStateAction,
-  createContext,
-  useContext,
+	type Dispatch,
+	type ReactNode,
+	type SetStateAction,
+	createContext,
+	useContext,
 } from 'react';
 import { IconContext } from 'react-icons';
 import { type ThemeType, useTheme } from 'src/hooks';
 
 type UIContextType = {
-  themeMode: ThemeType;
-  setThemeMode: Dispatch<SetStateAction<ThemeType>>;
+	themeMode: ThemeType;
+	setThemeMode: Dispatch<SetStateAction<ThemeType>>;
 };
 
 const UIContext = createContext<UIContextType | null>(null);
 UIContext.displayName = 'UIContext';
 
 function AppWrapper({ children }: { children: ReactNode }) {
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
-  return (
-    <App
-      // https://ant-design.antgroup.com/components/app-cn
-      style={{
-        height: '100%',
-        display: 'flex',
-        placeContent: 'center',
-        placeItems: 'center',
-        background: colorBgContainer,
-      }}
-    >
-      {children}
-    </App>
-  );
+	const {
+		token: { colorBgContainer },
+	} = theme.useToken();
+	return (
+		<App
+			// https://ant-design.antgroup.com/components/app-cn
+			style={{
+				height: '100%',
+				display: 'flex',
+				placeContent: 'center',
+				placeItems: 'center',
+				background: colorBgContainer,
+			}}
+		>
+			{children}
+		</App>
+	);
 }
 
 export function UIProvider({ children }: { children: ReactNode }) {
-  const { displayTheme, themeMode, setThemeMode } = useTheme();
+	const { displayTheme, themeMode, setThemeMode } = useTheme();
 
-  return (
-    <UIContext.Provider
-      value={{
-        themeMode,
-        setThemeMode,
-      }}
-    >
-      <IconContext.Provider
-        // react-icon config https://github.com/react-icons/react-icons#configuration
-        value={{ size: '1.2em' }}
-      >
-        <StyleProvider
-          // NOTE: https://github.com/ant-design/ant-design/issues/45560
-          hashPriority='high'
-        >
-          <ConfigProvider
-            theme={{
-              algorithm: displayTheme === 'dark' ? theme.darkAlgorithm : theme.defaultAlgorithm,
-              token: {
-                // colorPrimary: '#0053bc',
-                // borderRadius: 14,
-              },
-              components: {},
-            }}
-          >
-            <AppWrapper>{children}</AppWrapper>
-          </ConfigProvider>
-        </StyleProvider>
-      </IconContext.Provider>
-    </UIContext.Provider>
-  );
+	return (
+		<UIContext.Provider
+			value={{
+				themeMode,
+				setThemeMode,
+			}}
+		>
+			<IconContext.Provider
+				// react-icon config https://github.com/react-icons/react-icons#configuration
+				value={{ size: '1.2em' }}
+			>
+				<StyleProvider
+					// NOTE: https://github.com/ant-design/ant-design/issues/45560
+					hashPriority='high'
+				>
+					<ConfigProvider
+						theme={{
+							algorithm: displayTheme === 'dark' ? theme.darkAlgorithm : theme.defaultAlgorithm,
+							token: {
+								// colorPrimary: '#0053bc',
+								// borderRadius: 14,
+							},
+							components: {},
+						}}
+					>
+						<AppWrapper>{children}</AppWrapper>
+					</ConfigProvider>
+				</StyleProvider>
+			</IconContext.Provider>
+		</UIContext.Provider>
+	);
 }
 
 export function useUIContext() {
-  const context = useContext(UIContext);
-  if (!context) {
-    throw new Error('useUIContext must be used within <UIContext.Provider>');
-  }
-  return context;
+	const context = useContext(UIContext);
+	if (!context) {
+		throw new Error('useUIContext must be used within <UIContext.Provider>');
+	}
+	return context;
 }
--- src/routes/_dashboard.tsx
+++ src/routes/_dashboard.tsx
@@ -4,17 +4,17 @@ import { DashboardLayout } from 'src/layouts';
 import { isAuthenticated } from 'src/services';
 
 export const Route = createFileRoute('/_dashboard')({
-  beforeLoad: async ({ location }) => {
-    // https://tanstack.com/router/latest/docs/framework/react/guide/authenticated-routes
-    if (!(await isAuthenticated())) {
-      throw redirect({
-        to: '/signin',
-        search: {
-          redirect: location.href,
-        },
-      });
-    }
-  },
-  component: DashboardLayout,
-  pendingComponent: Spinner,
+	beforeLoad: async ({ location }) => {
+		// https://tanstack.com/router/latest/docs/framework/react/guide/authenticated-routes
+		if (!(await isAuthenticated())) {
+			throw redirect({
+				to: '/signin',
+				search: {
+					redirect: location.href,
+				},
+			});
+		}
+	},
+	component: DashboardLayout,
+	pendingComponent: Spinner,
 });
